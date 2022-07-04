23e582a90eba0bfddf4f0d8ced2fa14c53d819ef
IPocea <pocea.ionut@gmail.com>
2022-07-04 19:52:44 +0300

    add

--- backend/server.js
+++ backend/server.js
@@ -21,29 +21,29 @@ app.use(
 		httpOnly: true,
 	})
 );
-// const db = require("./app/models");
-// const Role = db.role;
-// db.sequelize.sync({ force: true }).then(() => {
-// 	console.log("Drop and Resync Db");
-// 	initial();
-// });
+const db = require("./app/models");
+const Role = db.role;
+db.sequelize.sync({ force: true }).then(() => {
+	console.log("Drop and Resync Db");
+	initial();
+});
 // db.sequelize.sync();
-// function initial() {
-// 	Role.create({
-// 		id: 1,
-// 		name: "user",
-// 	});
+function initial() {
+	Role.create({
+		id: 1,
+		name: "user",
+	});
 
-// 	Role.create({
-// 		id: 2,
-// 		name: "moderator",
-// 	});
+	Role.create({
+		id: 2,
+		name: "moderator",
+	});
 
-// 	Role.create({
-// 		id: 3,
-// 		name: "admin",
-// 	});
-// }
+	Role.create({
+		id: 3,
+		name: "admin",
+	});
+}
 // simple route
 app.get("/", (req, res) => {
 	res.json({ message: "Welcome to AsBijoux." });
